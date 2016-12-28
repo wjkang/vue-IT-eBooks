d408165e7825ea76c9bba71113d380d4fdbfbadf
Administrator <977865769@qq.com>
2016-12-28 12:14:25 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,40 @@
 var path=require("path");
 var webpack=require("webpack");
-module.exports={
-    
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'build.js'
+    },
+    module: {
+         loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    // vue-loader options go here
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.common.js'
+        }
+    }
+    //devtool: '#eval-source-map'
 }
