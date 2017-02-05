a1e794730f4490f05bdb477a2777f5b3e60f42e7
username <977865769@qq.com>
2017-02-05 12:56:48 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import VueRouter from 'vue-router'
+import store from './vuex/store'
+import Vuex from 'vuex'
 import App from './App.vue'
 
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+const routes = [
+    {
+
+    }
+];
 new Vue({
   el: '#app',
   render: h => h(App)
